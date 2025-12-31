9ac0c86a02a069befb45606aeee20681b37a06bf
dilan dhakal <dilandhakal889@gmail.com>
2025-12-31 22:19:28 +0545

    add code

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// console.log("hello world");
+
+// let a=23;
+// let b=33;
+
+// console.log("the sum of a and b is",a+b);
+
+// console.log(a==b);
+
+// let Array = ["car","bus","ven","truck"];
+// console.log(Array);
+// console.log(Array[2]);
+
+// let Person = {
+//     name : "Dilan Dhakal",
+//     age : 22,
+//     address : "Keurani"
+// }
+
+// console.log(Person);
+// console.log(Person.name);
+
+// let age=18;
+// if(age>18){
+//     console.log("Eligiable");
+// }else{
+//     console.log("Not eligiable");
+// }
+
+// let name = "Alexa";
+// if(name == "Alexa"){
+//     console.log("Hi! Alexa");
+// }else{
+//     console.log("Unathorize person");
+// }
+
+// let ab = 20;
+// console.log(typeof(a));
+
+
+
+// const math = 87;
+// const science = 50;
+// const english = 50;
+// const nepali = 60;
+// const computer = 67;
+
+// if(math >= 35 && science >=35 && english >=35 && nepali >= 35 && computer >=35){
+//     let total = math + science + english + nepali + computer;
+//     let percentage = total * 100/500;
+//     let gpa = percentage / 25;
+//     console.log("Total=", total);
+//     console.log("Percentage=", percentage);
+//     console.log("Gpa = ", gpa);
+// }
+// else {
+//     console.log("You are failed")
+// }
