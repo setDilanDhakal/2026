6a01506ba125b54a997eb6ad907376fdb94c3e63
dilan dhakal <dilandhakal889@gmail.com>
2026-02-10 17:13:50 +0545

    aadd code

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,30 +1,49 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 
 dotenv.config({
-    path:'./.env'
-})
+  path: "./.env",
+});
 
 const app = express();
 const port = process.env.PORT;
 
 
-app.use('/',(req,res)=>{
-    res.send("this is default page");
-})
 
-app.use('/home',(req,res)=>{
-    res.send('this is home page')
-})
-app.use('/about',(req,res)=>{
-    res.send('this is about page')
-})
-app.use('/contact',(req,res)=>{
-    res.send('this is contact page')
-})
+// app.use("/", (req, res) => {
+//   const result = {
+//     name: "dilan",
+//     age: 22,
+//   };
+//   res
+//     .status(201)
+//     .json({ status: true, message: "User is created", data: result });
+// });
 
-app.listen(port,()=>{
-    console.log("Server is running at",port)
+app.get("/user", (req, res) => {
+  const user = {
+    id: "123",
+    name: "Dilan",
+    email: "dilan@gmail.com",
+    role: "admin",
+    isVerified: true,
+  };
+
+  res
+    .status(200)
+    .json({ status: true, message: "User successfully send", data: user });
 });
 
+// app.use('/land',(req,res)=>{
+//     res.send([1,2,3,4])
+// })
+// app.use('/about',(req,res)=>{
+//     res.send('this is about page..')
+// })
+// app.use('/contact',(req,res)=>{
+//     res.send('this is contact page.')
+// })
 
+app.listen(port, () => {
+  console.log("Server is running at", port);
+});
